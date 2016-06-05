0bba0c6260e4b2c33045a5444ff732330b519554
Martin Geisler <martin@geisler.net>
2016-06-05 21:38:34 +0200

    tests: run "cram tests/*.t" via "go test"
    This integrates the self-tests with the normal "go test" mechanism,
    making it easier for everybody to remember to run these tests.

---
+++ self_test.go
@@ -0,0 +1,23 @@
+package cram
+
+import (
+	"os/exec"
+	"path/filepath"
+	"testing"
+)
+
+// TestSelf runs Cram on all .t files inside the tests directory.
+func TestSelf(t *testing.T) {
+	paths, err := filepath.Glob("tests/*.t")
+	if err != nil {
+		t.Fatal("Error while globbing:", err)
+	}
+
+	cmd := exec.Command("cram", paths...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Log("Cram failed:", err)
+		t.Log(string(output))
+		t.Fail()
+	}
+}
