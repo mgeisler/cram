8ba5c524c5cac478de5c81a40bd39d9879f2addf
Martin Geisler <martin@geisler.net>
2016-06-23 15:23:05 +0200

    fuzz: prepare runner for multiple fuzz tests

--- fuzz.sh
+++ fuzz.sh
@@ -4,24 +4,35 @@ echo "Building go-fuzz"
 go get -v github.com/dvyukov/go-fuzz/go-fuzz
 go get -v github.com/dvyukov/go-fuzz/go-fuzz-build
 
-echo "Instrumenting Cram"
-go-fuzz-build github.com/mgeisler/cram/fuzz/ParseTest
-
+# This test has no fixed corpus, we use our test cases.
 cp -a tests fuzz/ParseTest/corpus
 
-echo "Starting fuzz test"
-timeout -s INT 25 go-fuzz -bin ParseTest-fuzz.zip -workdir=fuzz/ParseTest
+fuzz_test () {
+    echo "Preparing $1 fuzz test"
+    go-fuzz-build github.com/mgeisler/cram/fuzz/$1
+
+    echo "Starting $1 fuzz test"
+    timeout -s INT 25 go-fuzz -bin $1-fuzz.zip -workdir=fuzz/$1
+
+    local return_value=0
+    for path in fuzz/$1/crashers/*.quoted; do
+        # This test is here to support dash
+        if [ -e "$path" ]; then
+            return_value=1
+            echo "Found crasher:"
+            cat "$path"
+            echo "Output:"
+            cat "${path%.quoted}.output"
+            echo
+        fi
+    done
+    return $return_value
+}
 
 exit_code=0
-for path in fuzz/ParseTest/crashers/*.quoted; do
-    # This test is here to support dash
-    if [ -e "$path" ]; then
+for t in ParseTest; do
+    if ! fuzz_test $t; then
         exit_code=1
-        echo "Found crasher:"
-        cat "$path"
-        echo "Output:"
-        cat "${path%.quoted}.output"
-        echo
     fi
 done
 
